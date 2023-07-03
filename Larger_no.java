a29cbef7aa58f040763a8782f5569286dee27e0e
25Mansijadhav <123803719+25Mansijadhav@users.noreply.github.com>
2023-07-03 23:12:26 +0530

    Add files via upload

---
+++ Larger_no.java
@@ -0,0 +1,24 @@
+public class Larger_no {
+    public static void main(String[] args) {
+        int A = 28;
+        int B = 9;
+        int C = 55;
+
+        if (A >= B) {
+            if (A >= C) {
+                System.out.println(A);
+            } else {
+                System.out.println(C);
+            }
+
+        } else {
+            if (B >= C) {
+                System.out.println(B);
+            } else {
+                System.out.println(C);
+            }
+        }
+
+    }
+
+}
