8fffdf918d8fcbe18fea5e64adc94823bca34c7a
25Mansijadhav <123803719+25Mansijadhav@users.noreply.github.com>
2023-07-03 23:12:46 +0530

    Add files via upload

---
+++ varAss.java
@@ -0,0 +1,11 @@
+public class varAss {
+    public static void main(String[] args) {
+        int number = 10;
+        double pi = 3.14159;
+        String country = "India";
+
+        System.out.println("Number : " + number);
+        System.out.println("Pi : " + pi);
+        System.out.println("Country : " + country);
+    }
+}
